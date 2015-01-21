c64469ca226c10ca46df6172ec4bc1e72b46b0c7
Roman Schmid <bummzack@gmail.com>
2015-01-21 11:48:28 +0100

    Removed the "add existing" Searchfield from the default Gridfield, since it doesn't make sense to have it in a `has_many` relation (unless we'd want to "steal" blocks from other pages). Unlinking Blocks doesn't exist either, so the field can be removed without consequences.

--- code/extensions/PageBlocks.php
+++ code/extensions/PageBlocks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Extension that applies blocks to a page.
- * 
+ *
  * @author bummzack
  */
 class PageBlocks extends DataExtension
@@ -9,29 +9,29 @@ class PageBlocks extends DataExtension
 	private static $has_many = array(
 		'Blocks' => 'Block'
 	);
-	
+
 	/**
 	 * Whether or not the page should have a button to publish page & blocks with one click.
 	 * Set this via config.yml
 	 * @var bool
 	 */
 	private static $allow_publish_all = true;
-	
+
 	public function updateCMSFields(FieldList $fields) {
-		$gridConfig = singleton('Block')->has_extension('Sortable') 
+		$gridConfig = singleton('Block')->has_extension('Sortable')
 			? GridFieldConfig_BlockEditor::create('SortOrder')
 			: GridFieldConfig_BlockEditor::create();
-		
-		
-		$gridField = GridField::create('Blocks', _t('PageBlocks.BLOCK', 'Block', 'GridField Title'), 
+
+
+		$gridField = GridField::create('Blocks', _t('PageBlocks.BLOCK', 'Block', 'GridField Title'),
 			$this->owner->Blocks(), $gridConfig);
 		$gridField->setModelClass('Block');
-		
+
 		$fields->addFieldsToTab('Root.Main', array(
 			$gridField
 		), 'Metadata');
 	}
-	
+
 	/**
 	 * Add the publish all button
 	 */
@@ -39,10 +39,10 @@ class PageBlocks extends DataExtension
 		if(Config::inst()->get('PageBlocks', 'allow_publish_all') == false){
 			return;
 		}
-		$button = FormAction::create('publishblocks', 
+		$button = FormAction::create('publishblocks',
 			_t('PageBlocks.PUBLISH_ALL', 'Publish Page & Blocks', 'Button label to publish page and all blocks'))
 			->setAttribute('data-icon', 'accept');
-		
+
 		if($majorActions = $actions->fieldByName('MajorActions')){
 			$majorActions->push($button);
 		} else {
@@ -64,7 +64,7 @@ class GridFieldConfig_BlockEditor extends GridFieldConfig_RelationEditor {
 	 */
 	public function __construct($sortField = null, $itemsPerPage=null) {
 		parent::__construct($itemsPerPage);
-		
+
 		// setup a bulk manager for block management
 		$bulkManager = new GridFieldBulkManager();
 		// remove the default actions
@@ -75,21 +75,30 @@ class GridFieldConfig_BlockEditor extends GridFieldConfig_RelationEditor {
 				$bulkManager->removeBulkAction($key);
 			}
 		}
-		
+
 		// add the actions in desired order
 		$bulkManager
 			->addBulkAction('publish', _t('PageBlock.PUBLISH', 'Publish'))
 			->addBulkAction('unpublish', _t('PageBlock.UNPUBLISH', 'Unpublish'))
 			->addBulkAction('bulkedit', _t('PageBlock.EDIT', 'Edit'), 'GridFieldBulkActionEditHandler')
 			->addBulkAction('versioneddelete', _t('PageBlock.DELETE', 'Delete'),'GridFieldBulkActionVersionedDeleteHandler');
-		
+
 		if($sortField && class_exists('GridFieldOrderableRows')){
 			$this->addComponent(new GridFieldOrderableRows($sortField));
 		}
-		
+
 		// remove the delete action, since unlinking is not required
 		$this->removeComponent($this->getComponentByType('GridFieldDeleteAction'));
-		
+
+		// remove the search field since it doesn't make sense (cannot add existing, unless "stealing" from another page)
+		$this->removeComponentsByType('GridFieldAddExistingAutocompleter');
+
+		/*
+		$searchButton = new GridFieldAddExistingSearchButton('buttons-before-right');
+		$searchButton->setSearchList(Block::get()->filter(array('ParentID' => 0)));
+		$this->addComponent($searchButton);
+		*/
+
 		$this->addComponent(new GridFieldAddNewMultiClass(), 'GridFieldToolbarHeader');
 		$this->addComponent($bulkManager);
 		$this->getComponentByType('GridFieldDataColumns')->setDisplayFields(array(
